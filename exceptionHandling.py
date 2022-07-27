0f9013d06de2c7e2a2573a7bda7870b126af3cf5
krish-alt <83361261+krish-alt@users.noreply.github.com>
2022-07-27 23:11:34 +0530

    update exceptionHandling.py

--- exceptionHandling.py
+++ exceptionHandling.py
@@ -1,10 +1,54 @@
 # try...except...else...finally
+# try:
+#     print(5/8)
+# except ZeroDivisionError:
+#     print("You can't divide by zero!")
+# else:
+#     print("5 divided by 0 is: ", 5/0)
+# finally:
+#     print("Executing finally clause")
+# # #############################################################################
+
+# a = 1
+
+# try:
+#     b = int(input("Please enter a number to divide a"))
+#     a = a/b
+#     print("Success a=",a)
+# except ZeroDivisionError: # we can name the error or use the name of the error
+#     print("You can't divide by zero!")
+# finally:
+#     print("Executing finally clause")
+
+#################################################################################
+
+# a = 1
+
+# try:
+#     b = int(input("Please enter a number to divide a"))
+#     a = a/b
+#     print("Success a=",a)
+# except ZeroDivisionError:
+#     print("The number you provided cant divide 1 because it is 0")
+# except ValueError:                              #when we put a string instead of a number
+#     print("You did not provide a number") 
+# except:
+#     print("Something went wrong")
+
+##################################################################################
+
+a = 1
+
 try:
-    print(5/8)
+    b = int(input("Please enter a number to divide a"))
+    a = a/b
 except ZeroDivisionError:
-    print("You can't divide by zero!")
-else:
-    print("5 divided by 0 is: ", 5/0)
+    print("The number you provided cant divide 1 because it is 0")
+except ValueError:
+    print("You did not provide a number")
+except:
+    print("Something went wrong")
+else:                           #else is executed when no error is raised in the try block
+    print("success a=",a)
 finally:
-    print("Executing finally clause")
-# #############################################################################
+    print("Processing Complete")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,12 @@
-print(len(tuple1))
+a = 1
+
+# try:
+#     b = int(input("Please enter a number to divide a"))
+#     a = a/b
+#     print("Success a=",a)
+# except ZeroDivisionError:
+#     print("The number you provided cant divide 1 because it is 0")
+# except ValueError:                              #when we put a string instead of a number
+#     print("You did not provide a number") 
+# except:
+#     print("Something went wrong")
